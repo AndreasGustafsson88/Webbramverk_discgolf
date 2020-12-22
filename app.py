cc4ba30989c93bda9050ee2972cec3449a3fb1d8
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-12-22 13:53:08 +0100

    fixes

--- app.py
+++ app.py
@@ -1,32 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__,
-            static_url_path="",
-            static_folder="")
-
-
-@app.route('/')
-def index():
-
-    return render_template("index.html")
-
-
-@app.route('/signup')
-def signup():
-
-    return render_template("signup.html")
-
-
-@app.route('/courses')
-def courses():
-
-    return render_template("courses.html")
-
-
-@app.route('/profile_page')
-def profile_page():
-
-    return render_template("profile_page.html")
+from App.UI import app
 
 if __name__ == '__main__':
     app.run(debug=True)
