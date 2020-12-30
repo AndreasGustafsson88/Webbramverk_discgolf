ae52fa79d89a28c03989c770e4c449dcc552ef91
William Åslund <aslund.william@hotmail.com>
2020-12-30 16:29:11 +0100

    skrivit protokoll och kod 30/12

---
+++ App/Data/Repository/courses_repo.py
@@ -0,0 +1,11 @@
+from App.Data.Models.courses import Course
+
+
+
+def get_all_names():
+    return [course.name for course in Course.all()]
+
+def get_one_course(name):
+    return Course.find(name=name).first_or_none()
+
+
