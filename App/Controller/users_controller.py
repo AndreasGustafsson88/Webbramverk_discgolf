f8e45916f0f77fe05341fdd6f5ada3cd165e699c
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2021-01-05 09:15:48 +0100

    added method player_hcp for calculating nr of throws for each hole

--- App/Controller/users_controller.py
+++ App/Controller/users_controller.py
@@ -1,4 +1,9 @@
 from App.Data.Repository import users_repo as ur
 
+
 def get_all_friends(current_user):
     return ur.get_all_friends(current_user)
+
+
+def get_users(users):
+    return ur.get_users(users)
--- App/Data/Models/users.py
+++ App/Data/Models/users.py
@@ -3,3 +3,33 @@ from App.Data import Document, db
 
 class User(Document):
     collection = db.users
+
+    def player_hcp(self, course):
+
+        hole_average = sorted([{"hole": i, "average": "{:.2f}".format(hole["average"] - hole["Par"]), "strokes": 0}
+                               for i, hole in enumerate(course.holes[1:], 1)], key=lambda x: x["average"])
+
+        if self.rating is None:
+            return hole_average
+
+        total_throws = 0
+
+        for k, v in course.rating.items():
+            if v >= self.rating:
+                total_throws += int(k) + 1
+                break
+
+        difference = total_throws - course.course_par
+
+        if difference == 0:
+            return sorted(hole_average, key=lambda x: x["hole"])
+
+        elif difference < 0:
+            for i in range(1, difference * -1 + 1):
+                hole_average[-i % course.holes[0]]["strokes"] -= 1
+
+        elif difference > 0:
+            for i in range(difference):
+                hole_average[i % course.holes[0]]["strokes"] += 1
+
+        return sorted(hole_average, key=lambda x: x["hole"])
